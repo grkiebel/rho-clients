e978b4aa1a5c63fc25545ebcae9675e9edb96d02
grk <grkiebel@gmail.com>
2024-07-14 10:35:41 -0700

    created ops helpers.py

--- rho_clients/generator/template_ops.py
+++ rho_clients/generator/template_ops.py
@@ -1,34 +1,8 @@
 from ..api import g_api as apx
 from ..log_config import get_logger
 from ..cmds.sim import Sim
+from ..ops.helpers import display_ops_result
 
 sim = Sim()
 
 apx.initialize("http://localhost:8080", get_logger("API-Access"))
-
-
-def verify_service_status():
-    try:
-        response = apx.read_root()
-        print(response)
-        return True
-    except Exception as e:
-        print(e)
-        return False
-
-
-def display_ops_result(obj):
-    if obj is None:
-        print("None")
-        return
-    obj_type = type(obj)
-    if obj_type == list:
-        for item in obj:
-            print(vars(item))
-    elif obj_type == dict:
-        print(obj)
-    elif obj_type == apx.Outcome:
-        print(obj)
-    else:
-        for attr, value in vars(obj).items():
-            print(f"{attr}: {value}")
---
+++ rho_clients/ops/helpers.py
@@ -0,0 +1,28 @@
+from ..api import g_api as apx
+
+
+def display_ops_result(obj):
+    if obj is None:
+        print("None")
+        return
+    obj_type = type(obj)
+    if obj_type == list:
+        for item in obj:
+            print(vars(item))
+    elif obj_type == dict:
+        print(obj)
+    elif obj_type == apx.Outcome:
+        print(obj)
+    else:
+        for attr, value in vars(obj).items():
+            print(f"{attr}: {value}")
+
+
+def verify_service_status():
+    try:
+        response = apx.read_root()
+        print(response)
+        return True
+    except Exception as e:
+        print(e)
+        return False
