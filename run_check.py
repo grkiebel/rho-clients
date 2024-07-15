1454dede0efc6b34341e0633bab59854135cd0d3
grk <grkiebel@gmail.com>
2024-07-15 14:18:15 -0700

    working on run_check

--- run_check.py
+++ run_check.py
@@ -1,51 +1,51 @@
 from time import sleep
 from typing import List
 from threading import Thread
-from access import api_access as api_axs
-from access import (
-    MatchCheckerBase,
-    WorkFinder,
-    TaskCreateRep,
-    ToolCreateRep,
-    WorkCreateRep,
-    verify_service_status,
-)
-from app_models import AppMatchChecker
-from sims import ToolWorker, SimTool, SimTask
+from rho_clients.log_config import get_logger
+from rho_clients.api import g_api as apx
+from rho_clients.client_apps import app_models as cam
+from rho_clients.client_apps.assigner_app import WorkFinder
+from rho_clients.client_apps.tool_app import ToolWorker
+from rho_clients.cmds.sim import Sim, make
+
+
+apx.initialize("http://localhost:8080", get_logger("API-Access"))
+
+sim = Sim()
 
 
 def clear_db():
-    outcome = api_axs.delete_all_tools()
+    outcome = apx.delete_all_tools()
     print(outcome)
-    outcome = api_axs.delete_all_tasks()
+    outcome = apx.delete_all_tasks()
     print(outcome)
-    outcome = api_axs.delete_all_reports()
+    outcome = apx.delete_all_reports()
     print(outcome)
-    outcome = api_axs.delete_all_work()
+    outcome = apx.delete_all_work()
     print(outcome)
-    outcome = api_axs.delete_all_archived_work()
-    print(outcome)
-
-
-def make_tool(tool: SimTool):
-    tool_create_rep = ToolCreateRep(tool_id=tool.tool_id, tool_skills=tool.tool_skills)
-    outcome = api_axs.create_tool(tool_create_rep)
+    outcome = apx.delete_all_archived_work()
     print(outcome)
 
 
-def make_task(task: SimTask) -> None:
-    task_create_rep = TaskCreateRep(task_id=task.task_id, task_needs=task.task_needs)
-    outcome = api_axs.create_task(task_create_rep)
-    print(outcome)
+# def make_tool(tool: SimTool):
+#     tool_create_rep = apx.ToolCreate(tool_id=tool.tool_id, tool_skills=tool.tool_skills)
+#     outcome = apx.create_tool(tool_create_rep)
+#     print(outcome)
+
+
+# def make_task(task: SimTask) -> None:
+#     task_create_rep = apx.TaskCreate(task_id=task.task_id, task_needs=task.task_needs)
+#     outcome = apx.create_task(task_create_rep)
+#     print(outcome)
 
 
 def assign_work() -> None:
-    work_finder = WorkFinder(
-        match_checker=AppMatchChecker()
+    work_finder = cam.WorkFinder(
+        match_checker=cam.AppMatchChecker()
     )  # just use MatchCheckerBase for "match all"
     for tool_id, task_id in work_finder.pairs:
-        work_create_rep = WorkCreateRep(tool_id=tool_id, task_id=task_id)
-        outcome = api_axs.create_work(work_create_rep)
+        work_create_rep = apx.WorkCreate(tool_id=tool_id, task_id=task_id)
+        outcome = apx.create_work(work_create_rep)
         print(outcome.message)
 
 
@@ -70,7 +70,7 @@ def run_assigner_in_thread() -> Thread:
 
 def run_archive_monitor():
     while True:
-        archived_work = api_axs.get_all_archived_work()
+        archived_work = apx.get_all_archived_work()
         ids = [str(archive.work_id) for archive in archived_work]
         recent = ids[-3:]
         print(f"Archived work: Num: {len(ids)} ,{', '.join(recent)}")
@@ -87,24 +87,25 @@ def run_archive_monitor_in_thread():
 class SimRun:
 
     def __init__(self, num_tools: int = 5, num_tasks: int = 10):
-        self.sim_tools: List[SimTool] = [SimTool() for _ in range(num_tools)]
-        self.sim_tasks: List[SimTask] = [SimTask() for _ in range(num_tasks)]
+        self.tool_creates: List[apx.ToolCreate] = sim.make_tool_create_list(num_tools)
+        self.task_creates: List[apx.TaskCreate] = sim.make_task_create_list(num_tasks)
+
         self.threads: List[Thread] = []
 
     def run(self):
         clear_db()
 
-        for tool in self.sim_tools:
-            make_tool(tool)
+        for tool_create in self.tool_creates:
+            apx.tool_create(tool_create)
 
-        for SimTask in self.sim_tasks:
-            make_task(SimTask)
+        for task_create in self.task_creates:
+            apx.task_create(task_create)
 
         thread = run_assigner_in_thread()
         self.threads.append(thread)
 
-        for tool in self.sim_tools:
-            thread = run_tool_in_thread(tool.tool_id)
+        for tool_create in self.tool_creates:
+            thread = run_tool_in_thread(tool_create.tool_id)
             self.threads.append(thread)
 
         thread = run_archive_monitor_in_thread()
@@ -113,8 +114,8 @@ class SimRun:
 
 if __name__ == "__main__":
 
-    if not verify_service_status():
-        exit()
+    status = apx.general_status()
+    print(status)
 
     SimRun(num_tasks=20, num_tools=10).run()
 
