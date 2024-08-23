4126ddba22da3ddc9154f2784d1d729afa16aee1
grk <grkiebel@gmail.com>
2024-08-23 09:42:16 -0700

    progress

--- rho_clients/payload/tool_app.py
+++ rho_clients/payload/tool_app.py
@@ -1,7 +1,7 @@
 import random
 from time import sleep
 from typing import List
-from .tool_base import WorkContext, run_as_tool
+from ..client_apps.tool_base import WorkContext, run_as_tool
 from ..cmd_shell import cmd_helpers as hp
 from ..generated import g_api as apx
 
